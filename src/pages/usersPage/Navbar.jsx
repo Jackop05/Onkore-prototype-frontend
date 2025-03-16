15c6cb4da5e5cf7d06d1021a796e42c2ea9fe5c3
Jackop05 <jakubnyrbotzs@gmail.com>
2025-03-16 22:43:52 +0100

    Commit

--- src/pages/usersPage/Navbar.jsx
+++ src/pages/usersPage/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await fetch(`${apiUrl}/api/user/logout-user`, {
-        method: "GET",
+        method: "POST",
         credentials: "include",
       });
 
